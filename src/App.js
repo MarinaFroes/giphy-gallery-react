86652218578684f8638552cbbffcc45eb875ee5d
MFAC <ninafroes@gmail.com>
2019-08-09 17:27:43 +0200

    feat: Remove SearchBar and GifGallery and add Main component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import SearchBar from './components/SearchBar';
-import GifGallery from './components/GifGallery';
-import styled from 'styled-components';
-
-const Main = styled.main`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
+import Main from './components/Main';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <Main>
-        <SearchBar />
-        <GifGallery />
-      </Main>
+      <Main />
       <Footer />
     </div>
   );
