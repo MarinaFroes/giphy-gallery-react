cce05a3e179b55d5fc52c47c1eb4984742aae044
MFAC <ninafroes@gmail.com>
2019-08-09 17:26:25 +0200

    feat: Add form element and onSubmit prop

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const SearchBarContainer = styled.div`
+const SearchBarContainer = styled.form`
   display: flex;
   align-items: left;
   border: 3px solid purple;
@@ -31,10 +31,11 @@ const InputSubmit = styled.input`
   }
 `;
 
-export default function SearchBar(): JSX.Element {
+export default function SearchBar({ onSubmit }) {
   return (
-    <SearchBarContainer>
-      <InputText type="text" name="search" id="search" />
+    <SearchBarContainer onSubmit={onSubmit}>
+      <InputText type="text" name="search" id="search"
+        placeholder="Keyword..."/>
       <InputSubmit type="submit" value="Search" />
     </SearchBarContainer>
   )
