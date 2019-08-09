25b6ae103f888d54f76df37bf68b9f05ef896704
MFAC <ninafroes@gmail.com>
2019-08-09 15:48:16 +0200

    feat: Import components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import SearchBar from './components/SearchBar';
+import GifGallery from './components/GifGallery';
+import styled from 'styled-components';
 
-function App() {
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default function App() {
   return (
     <div className="App">
+      <Header />
+      <Main>
+        <SearchBar />
+        <GifGallery />
+      </Main>
+      <Footer />
     </div>
   );
 }
-
-export default App;
